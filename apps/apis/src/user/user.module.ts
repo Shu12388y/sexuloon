25f8cd050d37f1a5c75ab66b5744173ee8d725e5
shu12388y <spl.sp999@gmail.com>
2025-10-20 17:52:56 +0530

    update the schema and added new services

--- apps/apis/src/user/user.module.ts
+++ apps/apis/src/user/user.module.ts
@@ -3,11 +3,13 @@ import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, userSchema } from 'schemas/user.schema';
 import { TwilioModule } from 'src/twilio/twilio.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: userSchema }]),
     TwilioModule,
+    JwtModule,
   ],
   providers: [UserService],
   exports: [UserService],
